4fc08c3299ec3d6fbbc9b862a54fb968c150894d
Shahed Rahim <shahed21@gmail.com>
2021-10-04 02:05:15 -0500

    added test for correct candidate values

--- test/election.js
+++ test/election.js
@@ -1,6 +1,9 @@
 var Election = artifacts.require("./Election.sol");
+var electionInstance;
 
 contract("Election", function(accounts) {
+    var electionInstance;
+
     it("initializes with two candidates", function() {
         return Election.deployed().then(function(instance) {
             return instance.candidatesCount();
@@ -8,4 +11,21 @@ contract("Election", function(accounts) {
             assert.equal(count, 2);
         });
     });
+
+    it("initializes the candidates with the correct values", function() {
+        return Election.deployed().then(function(instance) {
+            electionInstance = instance;
+            return instance.candidates(1);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 1, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct number of votes");
+            return electionInstance.candidates(2);
+        }).then(function(candidate) {
+            assert.equal(candidate[0], 2, "contains the correct id");
+            assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+            assert.equal(candidate[2], 0, "contains the correct number of votes");
+        });;
+    });
+
 });
