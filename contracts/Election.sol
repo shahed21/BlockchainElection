02d72eb3ab948e9462fb53ded8b308dba5205d42
Shahed Rahim <shahed21@gmail.com>
2021-10-04 01:45:31 -0500

    Election.sol now added candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,10 +1,26 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    //state variable
-    //declaring a state variable public creates its own getter function candidate()
-    string public candidate;
+    // Candidate struct
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // hash map to hold the candidates
+    mapping (uint => Candidate) public candidates;
+
+    // this integer keeps track of total candidates in the blockchain app
+    uint public candidatesCount;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
     }
 }
